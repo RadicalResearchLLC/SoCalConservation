2e56dafdf1aa7ef1b68db0ab463dca35a1c4fbec
Michael McCarthy <mikem@radicalresearch.llc>
2024-11-25 15:34:16 -0800

    First commit

---
+++ 1_acquire_process.R
@@ -0,0 +1,112 @@
+#30x30 processing
+#Created by Mike McCarthy
+#First created July 2024
+#Modified November 2024
+#Created for SoCal Earth - Redford Conservancy
+
+#30x30 definition
+#Areas under government ownership or control, primarily designated to protect species or habitats
+#Areas under perpetual easements that protect species and their habitats
+#Areas with species and habitat protection designations that have gone through a formal rulemaking or other
+#enforceable decision making process not subject to simple reversal
+
+library(sf)
+library(leaflet)
+library(tidyverse)
+library(tigris)
+
+'%ni%' <- Negate('%in%') ## not in operator
+
+#Directories
+wd <- getwd()
+data_dir <- str_c(wd, '/data')
+#don't use spherical geometry for joins
+sf_use_s2(FALSE)
+
+#Counties in SoCal
+SoCal_counties <- c('Los Angeles', 'Orange', 'Riverside', 'San Bernardino',
+                    'San Diego', 'Imperial', 'Ventura')
+
+### Import jurisdictions and create summary list
+#County data from tigris 
+CA_counties <- counties(state = 'CA', cb = T, year = 2023) |> 
+  filter(NAME %in% c(SoCal_counties)) |> 
+  st_transform(crs = 4326) |> 
+  select(NAME, ALAND) |> 
+  mutate(NAME = str_c(NAME, ' County'))
+
+#Places boundaries from tigris 2023
+#Cities and census designated places (and two towns?)
+jurisdictions <- places(state = 'CA', cb = T, year = 2023) |> 
+  st_transform(crs = 4326) |> 
+  st_filter(CA_counties , .predicate = st_covered_by) |> 
+  select(NAME, ALAND)
+  
+## create SoCal
+SoCal_summary <- CA_counties |> 
+  summarize(ALAND = sum(ALAND)) |> 
+  mutate(NAME = 'Southern California') 
+
+# create full Jurisdiction list - all in NAD83
+jurisFinal2 <- bind_rows(jurisdictions, CA_counties, SoCal_summary)
+
+rm(ls = CA_counties, jurisdictions, SoCal_summary)
+
+#30x30 terrestrial data from here - https://www.californianature.ca.gov/pages/open-data
+#2024 terrestrial dataset acquired November 2024
+#Check for update biannually
+conservationAreas <- sf::st_read(dsn = str_c(data_dir, '/30x30_Conserved_Areas_2024')) |> 
+  filter(CA_County_ %in% SoCal_counties) |> # filter down to 
+  st_make_valid() |> #make sure polygons are valid
+  st_zm() |> # remove z dimensions 
+  select(UNIT_NAME, Acres, geometry)
+
+## Group polygons by conservation name
+conserve1 <- conservationAreas |> 
+  select(UNIT_NAME, Acres, geometry) |> 
+  group_by(UNIT_NAME) |> 
+  summarize() 
+
+## Simplify polygons to 10 meter tolerance to conserve space
+conserve_simple <- conserve1 |> 
+  st_simplify(dTolerance = 10, preserveTopology = FALSE) |> 
+  st_make_valid() |> 
+  st_transform(crs = 4326)
+
+# Check how much smaller it is
+round(c(object.size(conservationAreas), object.size(conserve_simple)) / 1024)
+
+rm(ls = conservationAreas, conserve1)
+gc()
+
+# import biodiversity dataset
+# data from 30x30 open data
+# https://www.californianature.ca.gov/pages/open-data downloaded November 2024
+biodiversity <- sf::st_read(dsn = str_c(data_dir, '/Species_Biodiversity_-_ACE')) |> 
+  st_transform(crs = 4326) |> 
+  filter(County %in% str_to_upper(SoCal_counties))
+
+# simplify it by only keeping three columns and reduce polygon resolution
+bio_simpler <- biodiversity |> 
+  select(County, Name, SpBioRnkEc) |> 
+  group_by(County, Name, SpBioRnkEc) |> 
+  summarize(.groups = 'drop') |> 
+  st_simplify(dTolerance = .0003, preserveTopology = FALSE) # Tolerance is based on WGS84 decimal degrees
+
+# keep high biodiversity for filtering
+high_bio <- bio_simpler |> 
+  filter(SpBioRnkEc >= 4) |> 
+  select(geometry) |> 
+  summarize()
+
+round(c(object.size(biodiversity), 
+        #object.size(bio_simple),
+        object.size(bio_simpler)) /1024) 
+
+rm(ls = biodiversity)
+
+setwd(str_c(wd, '/SoCalConservation'))
+
+save.image('.RData')
+setwd(wd)      
+save.image('.RData')
---
+++ SoCalConservation/app.R
@@ -0,0 +1,272 @@
+library(shiny)
+library(leaflet)
+library(bslib)
+library(bsicons)
+library(tidyverse)
+library(sf)
+library(lwgeom)
+
+deploy_date <- 'November 25, 2024.'
+version <- 'Alpha v0.3 deployed'
+data_conservation <- '2024 30x30 Conservation data and Biodiversity index rank from:'
+biodiversity_data <- 'https://www.californianature.ca.gov/pages/open-data'
+jurisdiction_data <- 'Jurisdiction data from US Census TIGER/Line 2023'
+
+sf_use_s2(FALSE)
+
+# Define UI for application that draws a histogram
+ui <- page_sidebar(
+  #tags$style(type="text/css", "div.info.legend.leaflet-control br {clear: both;}"),
+  title = shiny::img(height = 60, src = 'https://www.socalearth.org/wp-content/uploads/2024/07/Bright-Background-Horizontal-1024x520.png'),
+  sidebar = sidebar(
+      width = 400,
+      title = 'Inputs',
+      selectizeInput(
+        inputId = 'Jurisdiction',
+        label = 'Select Jurisdiction',
+        choices = c(sort(jurisFinal2$NAME)),
+        selected = 'Southern California',
+        multiple = FALSE
+        ),
+      actionButton(inputId = 'Reset', label = 'Reset to full extent'),
+      sliderInput(inputId = 'Buffer',
+                  label = 'Accessible open space distance in miles',
+                  min = 0.5, max = 5, 
+                  step = 0.5, 
+                  value = 1),
+      textOutput('conservedSpace'),
+      hr(),
+      p(version, deploy_date), 
+      uiOutput('link'),
+      p(jurisdiction_data)
+     
+  ),
+       # Show a plot of the generated distribution
+  layout_columns(
+    uiOutput('consvdSpace'),
+    uiOutput('accessSpace'),
+    uiOutput('bioSpace')
+    ),
+  card(
+     shinycssloaders::withSpinner(leafletOutput('map', height =  600))
+     )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+
+ url <- a('30x30 California Open Data',
+        href = 'https://www.californianature.ca.gov/pages/open-data')
+ 
+ output$link <- renderUI({
+   tagList("30x30 Conservation data and Biodiversity index from:", url)
+ })
+  
+##Basic Map
+    output$map <- renderLeaflet({
+      leaflet() |> 
+        addProviderTiles(provider = providers$Esri.WorldShadedRelief, group = 'Topography') |>
+        addProviderTiles(provider = providers$Esri.WorldGrayCanvas, group = 'Basemap') |>
+        addProviderTiles(provider = providers$Esri.WorldImagery, group = 'Imagery') |> 
+        setView(lat = 34, lng = -117.30, zoom = 8) |> 
+          addLayersControl(baseGroups = c('Topography', 'Basemap', 'Imagery'),
+                       overlayGroups = c('Conserved lands',
+                                           'Jurisdiction',
+                                           'Biodiversity',
+                                           'Counties'), 
+                         options = layersControlOptions(collapsed = FALSE),
+                         position = 'topright'
+        ) |> 
+        hideGroup(c('Biodiversity', 'Counties')) |>
+        addMapPane('Jurisdiction', zIndex = 405)  |>  
+        addMapPane('Conserved lands', zIndex = 425) |> 
+        addMapPane('Biodiversity', zIndex = 415) |> 
+        addMiniMap(position = 'bottomleft')
+    })
+
+##Conservation layer
+observe({    
+  leafletProxy("map", data = conserve_simple)  |> 
+    clearGroup(group = 'Conserved lands')  |>
+    addPolygons(
+      color = 'darkgreen',
+      weight = 1,
+      fillOpacity = 0.5,
+      label = ~UNIT_NAME,
+      group = 'Conserved lands',
+      options = pathOptions(pane = 'Conserved lands'))
+})
+
+##Jurisdiction layer
+observe({ 
+  leafletProxy('map') |> 
+    clearGroup(group = 'Jurisdiction')  |>
+      addPolygons(
+        data = jurisSelected(),
+        color = 'black', 
+        weight = 2,
+        fillOpacity = 0.3,
+        group = 'Jurisdiction',
+        options = pathOptions(pane = 'Jurisdiction')) |> 
+      addPolylines(
+       data = bufferJ(),
+       color = 'darkorange',
+       weight = 3,
+       fillOpacity = 0.5,
+       group = 'Jurisdiction') 
+  })
+
+##County layer
+observe({ 
+  leafletProxy('map', data = CA_counties) |>
+    clearGroup(group = 'Counties') |> 
+      addPolylines(
+        color = 'black',
+        weight = 1,
+        group = 'Counties')
+})
+
+##Biodiversity
+pal2 <- colorFactor(domain = bio_simpler$SpBioRnkEc,
+                    palette = 'Purples')
+
+observe({ 
+  leafletProxy('map') |>
+    clearGroup(group = 'Biodiversity') |> 
+    addPolygons(
+      data = bio_simpler,
+      color = ~pal2(SpBioRnkEc),
+      weight = 0.1,
+      fillOpacity = 0.5,
+      group = 'Biodiversity',
+      options = pathOptions(pane = 'Biodiversity')) |> 
+    addLegend(
+      data = bio_simpler,
+      title = 'Biodiversity Rank',
+      pal = pal2,
+      values = ~SpBioRnkEc,
+      group = 'Biodiversity',
+      position = 'bottomright')
+})
+
+# choose jurisdiction 
+jurisSelected <- reactive({
+  #req(input$Jurisdiction) 
+  filter(jurisFinal2, NAME == input$Jurisdiction) 
+})
+
+### Calculations for three text boxes
+# calculate conserved space within jurisdiction
+space <- reactive({
+  areaJuris <- st_area(jurisSelected()) 
+  areaConsvd <- conserve_simple |> 
+    st_intersection(jurisSelected()) |> 
+    summarize() |> 
+    st_area()
+  pct_space <- round(100*areaConsvd/areaJuris, 1)
+  return(pct_space)
+})
+
+## Calculate buffer polygon
+bufferJ <- reactive({
+  jurisNAD83 <- st_transform(jurisSelected(), crs = 3310) #albers california
+  miles2km <- input$Buffer*1.6093*1000 #convert miles to meters
+  buffered <- st_buffer(jurisNAD83, miles2km) |> 
+    st_transform(crs = 4326) 
+  return(buffered)
+})
+
+# calculate accessible space within a distance
+bufferAreaOS <- reactive({
+  OS1 <- conserve_simple |> 
+    st_intersection(bufferJ()) |> 
+    summarize() |> 
+    st_area()
+  area1 <- st_area(bufferJ())
+  percent <- round(100*OS1/area1,1)
+  return(percent)
+})
+
+# calculate fraction of high biodiverse space conserved within jurisdiction
+
+bioPct <- reactive({
+  high_bio_poly <- high_bio |> 
+    st_intersection(jurisSelected()) # find any high_biodiversity area within jurisdiction
+  high_bio_area <- st_area(high_bio_poly) #calculate area of high biodiversity
+  consvd_high_bio_area <- conserve_simple |> 
+    st_intersection(jurisSelected()) |> 
+    summarize() |> 
+    st_intersection(high_bio_poly) |> 
+    st_area()
+  percentBio <- ifelse(length(consvd_high_bio_area) == 0,
+                    0,
+                    round(100*consvd_high_bio_area/high_bio_area,1)
+  )
+  return(percentBio)
+  #return(consvd_high_bio_area)
+  })
+
+output$consvdSpace <- renderUI({
+  bslib::value_box(
+    title = 'Conserved Open Space',
+    value = str_c(space(), '%'),
+    showcase = bs_icon('pie-chart'),
+  theme = 'yellow',
+  p('Land conserved as open space'),
+  p('Calculated as % of total land area')
+  )
+})
+
+#Accessible space textbox
+output$accessSpace <- renderUI({
+  bslib::value_box(
+    title = 'Accessible Open Space',
+    value = str_c(bufferAreaOS(), '%'),
+    showcase = bs_icon('bicycle'),
+    theme = 'orange',
+    p('Includes area within ', input$Buffer, ' mile(s)'),
+    p('Calculated as % of total area')
+    )
+})
+
+#Biodiversity space textbox
+
+output$bioSpace <- renderUI({
+  bslib::value_box(
+    title = 'High Biodiversity Open Space %',
+    value = str_c(bioPct(), '%'),
+    showcase = bs_icon('tree'),
+    theme = 'purple',
+    p('Percent of high biodiversity area'),
+    p('Calculated as % of high biodiversity area')
+  )
+})
+
+#create bounding box for map zoom
+bbox <- reactive({
+  bounds  <- as.numeric(st_bbox(jurisSelected()))
+})
+
+#observe extent of polygons if available
+observe({
+  leafletProxy('map', data = bbox()) |> 
+    fitBounds(
+      lng1 = bbox()[1], 
+      lat1 = bbox()[2], 
+      lng2 = bbox()[3], 
+      lat2 = bbox()[4]
+    )
+})
+
+observeEvent(input$Reset, {
+  updateSelectizeInput(session, 
+    "Jurisdiction",
+    label = 'Select Jurisdiction',
+    choices = c(jurisFinal2$NAME),
+    selected = 'Southern California')
+}
+)
+
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
